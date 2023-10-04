7a1d09ff976595d7b3fc1402962d3900b044db43
georgios-antoniadis <g.antoniadis21@gmail.com>
2023-10-04 19:24:15 +0300

    to change branch

--- evaluation_protocol/mape.py
+++ evaluation_protocol/mape.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 def mape(y_true, y_pred):
+    y_pred = y_pred.tolist()
+    y_true = y_true.tolist()
     n = len(y_true)
     mape_sum = 0
     for i in range(n):
--- evaluation_protocol/smape.py
+++ evaluation_protocol/smape.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 def smape(y_true, y_pred):
+    y_pred = y_pred.tolist()
+    y_true = y_true.tolist()
     n = len(y_true)
     smape_sum = 0
     for i in range(n):
