7ec49f77681e8e1676cac76410c6ed68b7f78314
georgios-antoniadis <g.antoniadis21@gmail.com>
2023-10-11 20:13:53 +0300

    html interactive

--- naive_methods/random_walk.py
+++ naive_methods/random_walk.py
@@ -5,7 +5,7 @@ def random_walk(input_data, test_set_length):
     # print(input_data[0])
     last_value = input_data[-1]
     current_value = last_value
-    noise_stddev = 0.1 * (max(input_data) - min(input_data))  # Adjust as needed
+    noise_stddev = 0.1 * (max(input_data) - min(input_data))
     predicted_values = []
     for i in range(test_set_length):
         predicted_values.append(current_value)
