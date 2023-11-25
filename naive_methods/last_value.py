69bc2d9b514e27919111df7fe3987cc64dbb61ab
georgios-antoniadis <g.antoniadis21@gmail.com>
2023-11-25 20:38:42 +0200

    deleting unecessary files

--- naive_methods/last_value.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-def last_value_naive(input_data, test_set_length):
-    last_value = input_data[-1]
-    naive_predictions = [last_value] * test_set_length
-    return naive_predictions
